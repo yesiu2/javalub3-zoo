c588de090fd56acb7a99d178c68bc6ea78389cf0
krrybarc <krystian.rybarczyk@assecobs.pl>
2017-11-05 18:44:17 +0100

    Created test for living bear
    (cherry picked from commit 96f3871)

--- src/test/java/pl/sdacademy/animals/bear/BearSpec.groovy
+++ src/test/java/pl/sdacademy/animals/bear/BearSpec.groovy
@@ -1,10 +1,24 @@
 package pl.sdacademy.animals.bear
 
+import org.joda.time.DateTime
+import pl.sdacademy.clock.Clock
 import spock.lang.Specification
 
 class BearSpec extends Specification {
 
+    def "Bear should be alive if has eaten within 10 days"() {
+        given:
+        Bear bear = testBear
+        bear.eat()
 
+        when:
+        def result = bear.isAlive()
 
+        then:
+        result == true
+
+        where:
+        testBear << [new BlackBear(5), new PolarBear(20)]
+    }
 
 }
