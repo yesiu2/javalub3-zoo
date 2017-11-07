9dc07b6cb3c9d8d3f897afb6d1589f8bc64316a1
krrybarc <krystian.rybarczyk@assecobs.pl>
2017-11-07 19:34:39 +0100

    Created working tests.

--- src/test/java/pl/sdacademy/animals/bear/BearTest.java
+++ src/test/java/pl/sdacademy/animals/bear/BearTest.java
@@ -1,6 +1,10 @@
 package pl.sdacademy.animals.bear;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BearTest {
@@ -10,8 +14,24 @@ class BearTest {
         Bear bear = new BlackBear(1);
         bear.eat();
 
+        assertThat(bear.isAlive()).isTrue();
+    }
+
+    @Test
+    void feedingBearShouldSetTheDateOfTheLastMealForNow() {
+        Bear bear = new BlackBear(1);
+        bear.eat();
+
+        assertThat(new Duration(bear.getLastMealTime(), DateTime.now()).isShorterThan(Duration.standardSeconds(1)))
+                .isTrue();
+    }
+
+    @Test
+    void bearShouldNotBeAliveIfItHasNotEatenForMoreThan10Days() {
+        Bear bear = new BlackBear(1, new TestClock());
+
         boolean result = bear.isAlive();
 
-        assertTrue(result == true);
+        assertThat(result).isFalse();
     }
 }
